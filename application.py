749731fe93ad43e02daf1ccf74f122266cdc56be
viswa44 <viswatej44@gmail.com>
2023-11-21 18:10:57 +0530

    modified deploy code

--- application.py
+++ application.py
@@ -21,13 +21,13 @@ def predict_datapoint():
         return render_template('home.html')
     else:
         data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
+            gender=request.form.get('gender'), # type: ignore
+            race_ethnicity=request.form.get('ethnicity'), # type: ignore
             parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            lunch=request.form.get('lunch'), # type: ignore
+            test_preparation_course=request.form.get('test_preparation_course'), # type: ignore
+            reading_score=float(request.form.get('writing_score')), # type: ignore
+            writing_score=float(request.form.get('reading_score')) # type: ignore
 
         )
         pred_df=data.get_data_as_data_frame()
